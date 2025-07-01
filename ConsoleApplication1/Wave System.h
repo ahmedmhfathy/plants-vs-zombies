6e103093c3a1ebcfc8cbe23c778991fdcec92ad6
Omar Hazem <ohazem126@gmail.com>
2025-07-01 18:27:32 +0300

    // clean code in waveSystem

--- ConsoleApplication1/Wave System.h
+++ ConsoleApplication1/Wave System.h
@@ -37,10 +37,9 @@ Texture Planttexttexture;
 Sprite Planttextsprite;
 Texture zombieinStreettex;
 Sprite zombieinStreet;
-SoundBuffer soundfogbuffer;
-SoundBuffer RSPSoundBuffer;
-SoundBuffer ZombiesAreComingBuffer;
-SoundBuffer WinSoundBuffer;
+Texture fogTex;
+Sprite fog;
+
 #pragma endregion
 
 #pragma region boolean
@@ -51,12 +50,19 @@ bool RSPSonudon = true;
 bool ZombiesAreComingSoundOn = true;
 #pragma endregion
 
-Texture fogTex;
-Sprite fog;
+#pragma region sound
+SoundBuffer soundfogbuffer;
+SoundBuffer RSPSoundBuffer;
+SoundBuffer ZombiesAreComingBuffer;
+SoundBuffer WinSoundBuffer;
+#pragma endregion
 
-
-
+#pragma region clock
 Clock clockRSP;
+#pragma endregion
+
+
+
 #pragma endregion
 
 #pragma region Textures and Sprites declaration
@@ -119,7 +125,7 @@ struct waves
 
 #pragma endregion
 
-int numofstartnormalzombie;
+
 //resets all data so you can retry the level
 void setupWaveData(bool isNight_) {
     isNightLevel = isNight_;
@@ -165,12 +171,7 @@ void setupWaveData(bool isNight_) {
 
 void startZombiePositions(int numZombies, int numberwave, int numlevel)
 {
-    //if (isNight && !onRoof) {
-    //    numofstartnormalzombie = numGraves;
-    //}
-    //else {
-    //    numofstartnormalzombie = 0;
-    //}
+    
 
     Plants_Zombies::StartZombies(numZombies, numlevel, onRoof);
 
